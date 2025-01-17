a308a7d68868a4b4c3dd9cc514dece05995d5ac5
frederic moras <frederik.moras@vlaanderen.be>
2025-01-17 22:24:48 +0100

    refactor(EventService): clean up code and improve consistency
    - Removed redundant comments in `getEventsForPeriod` for better readability.
    - Standardized single quotes for imports and error messages.
    - Updated formatting for improved code consistency and readability.

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,48 +1,36 @@
-import RestEventRepository from "../repository/RestEventRepository";
-
+import RestEventRepository from '../repository/RestEventRepository';
 
 export default class EventService {
-
     private eventRepository: RestEventRepository;
 
     constructor() {
         this.eventRepository = new RestEventRepository();
     }
 
-    /**
-     * Fetch and format events within a specific period, including past recurrences.
-     * @param start_date - The start date of the period (ISO string).
-     * @param end_date - The end date of the period (ISO string).
-     * @param collection - The collection name where events are stored.
-     * @returns Promise<EventFormData[]> - List of formatted events.
-     */
     async getEventsForPeriod(start_date: string, end_date: string): Promise<EventFormData[]> {
         try {
-
-            return   await this.eventRepository.fetchEventsForPeriod(start_date, end_date);
+            return await this.eventRepository.fetchEventsForPeriod(start_date, end_date);
         } catch (error) {
-            console.error("Error fetching events for period:", error);
+            console.error('Error fetching events for period:', error);
             throw error;
         }
     }
 
-    async changeDoneStatus(event:Event) {
+    async changeDoneStatus(event: Event) {
         const payload = {
             id: event.id,
             master_id: event.master_id,
             is_done: !event.is_done,
-            date: event.start_date
+            date: event.start_date,
         };
         try {
-          return await this.eventRepository.changeDoneStatus(payload).then((resp) => {
-              console.log('resp in service', resp);
-              return true;
-          });
-
+            return await this.eventRepository.changeDoneStatus(payload).then((resp) => {
+                console.log('resp in service', resp);
+                return true;
+            });
         } catch (error) {
-            console.error("Error changing done status:", error);
+            console.error('Error changing done status:', error);
             throw error;
         }
     }
-
 }
